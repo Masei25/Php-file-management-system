91a985a6c671c48b5f379f1e3ba1b8a6af7c53a7
Olu-Sammy <masei25@gmail.com>
2021-05-20 16:59:31 +0100

    db exported

--- uploaddb.sql
+++ uploaddb.sql
@@ -1,13 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 2.11.2.1
--- http://www.phpmyadmin.net
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 01, 2020 at 07:29 AM
--- Server version: 5.0.45
--- PHP Version: 5.2.5
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2021 at 05:58 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `uploaddb`
@@ -27,36 +35,29 @@ CREATE TABLE `admin` (
   `password` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `admin`
---
-
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `upload`
+-- Table structure for table `uploads`
 --
 
-CREATE TABLE `upload` (
-  `id` int(9) NOT NULL auto_increment,
-  `name` varchar(40) NOT NULL,
-  `file` varchar(40) NOT NULL,
-  `descript` varchar(40) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+CREATE TABLE `uploads` (
+  `id` int(10) NOT NULL,
+  `userid` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `file` varchar(20) NOT NULL,
+  `access_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `upload`
+-- Dumping data for table `uploads`
 --
 
-INSERT INTO `upload` (`id`, `name`, `file`, `descript`) VALUES
-(1, 'c.PNG', '', 'snapshot'),
-(3, 'Narendhar.doc', '', 'document'),
-(4, 'doc1.rtf.pdf', '', 'snapshot'),
-(6, 'Narendhar.doc', '', ''),
-(7, 'Narendhar.doc', '', ''),
-(9, '3.jpg', '', 'cars');
+INSERT INTO `uploads` (`id`, `userid`, `name`, `file`, `access_level`) VALUES
+(1, 3, 'Olu Sammy', '60a67ba97a570.jpg', 2),
+(2, 3, 'tea cup', '60a67d80cbc3a.jpg', 1),
+(3, 3, 'slash', '60a67eb5251bb.jpg', 1),
+(4, 3, 'Db', '60a681f7d61aa.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -65,19 +66,57 @@ INSERT INTO `upload` (`id`, `name`, `file`, `descript`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(9) NOT NULL,
-  `firstname` varchar(40) NOT NULL,
-  `lastname` varchar(40) NOT NULL,
-  `username` varchar(40) NOT NULL,
-  `password` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL,
+  `fullname` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`) VALUES
-(1, 'olu', 'sammy', 'sammy', '12345'),
-(0, 'mike', 'tyson', 'mikety', '678900'),
-(0, 'mike', 'tyson', 'mikety', '99070'),
-(0, 'olu', 'sam', 'sammyoo', '12345');
+INSERT INTO `users` (`id`, `fullname`, `username`, `email`, `password`) VALUES
+(2, 'Samuel Oluyede', 'masei25', 'admin@gmail.com', '$2y$10$n/KNURSSbFloPZgj2WEp/ui1cF4oOYVAcb0gvY.TupKqsVA6Jurji'),
+(3, 'Ham Tee', 'HamTee', 'hamtee@gmail.com', '$2y$10$2tLlLfRSnqC3QFN7dW10jOQX0f2oCcjHlh/xgU3V0l1/N3uMkZJv6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `uploads`
+--
+ALTER TABLE `uploads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `uploads`
+--
+ALTER TABLE `uploads`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
